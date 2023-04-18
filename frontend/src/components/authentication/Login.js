4ca2b0bc16c666cefd1de8e3f42d34872a46b8e4
unknown <y.rahul.14082002@gmail.com>
2023-04-18 22:40:31 +0530

    removed navigate

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
       });
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
-      navigate("mern-chat-app-production-2798.up.railway.app/chats");
+      navigate("/chats");
     } catch (error) {
       toast({
         title: "Error Occured!",
